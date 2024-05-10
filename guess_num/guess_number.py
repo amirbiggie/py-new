53ef406fb07a91c466af964ff2395175ee45991a
amirbiggie <amirrezamehralizadeh6@email.com>
2024-05-10 11:15:20 +0330

    find

---
+++ guess_num/guess_number.py
@@ -0,0 +1,41 @@
+# 2 ta  vorodi az user 
+# 5 bar try va check mikone 
+# 3 ta shart barqarar  kon 
+
+import random
+
+try:
+    num1 = int(input("enter low number: \n"))
+    num2 = int(input("enter high number: \n "))
+except:
+     print("please enter a valid number")    
+
+
+r = random.randint(num1,num2)  
+
+    
+
+guess_try = 5
+
+while guess_try > 0:
+     
+     try:
+         user_guess = int(input("enter your guess: \n"))
+        
+        
+         if r == user_guess:
+              print("good job, your win")
+              break;
+         elif r > user_guess:
+              print("your guess is lower than selectd number")
+         else: 
+              print("your guess is higher than selectd number")
+
+         guess_try -=1      
+    
+     except:
+         print("please enter a valid number")  
+
+print("by by")         
+   
+      
